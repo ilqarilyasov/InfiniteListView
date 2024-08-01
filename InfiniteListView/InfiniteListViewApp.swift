11fa53577f5ad173de694f3e013a39032853576c
Ilgar Ilyasov <iilyasov@awarex.com>
2024-08-01 11:13:51 -0400

    Initial Commit

---
+++ InfiniteListView/InfiniteListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteListViewApp.swift
+//  InfiniteListView
+//
+//  Created by Ilqar Ilyasov on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
